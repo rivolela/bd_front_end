79b4d81321a7a12d11c787d580e479c0b93cf2d1
rivolela <contato@pickoout.com.br>
2016-11-17 10:22:05 +0000

    change mongoose file

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,21 @@ var config = require('./config'),
 mongoose = require('mongoose');
 
 module.exports = function(){
-	var db = mongoose.connect(config.db);
-	require('../app/models/user.server.model');
-	require('../app/models/article.server.model');
+
+	//var uristring = config.db;
+	var uristring = config.db;
+
+	var db = mongoose.connect(uristring,function(err, res){
+		 if (err) {
+  			//console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  			console.log ('ERROR connecting to: ' + err);
+  		} else {
+  			//console.log ('Succeeded connected to: ' + uristring);
+  			console.log ('Data Base succeeded connected in env >> ', process.env.NODE_ENV);
+  			require('../app/models/user.server.model');
+			require('../app/models/article.server.model');
+  		}
+	});
+
 	return db;
 };

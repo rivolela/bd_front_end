4b5e7e34c099c1b81470dec84306e44fa093df92
rivolela <contato@pickoout.com.br>
2017-10-27 11:09:58 +0100

    change layout

--- config/seo/seo.js
+++ config/seo/seo.js
@@ -1,7 +1,9 @@
 module.exports = {
 
-	title: 'Decidaki - veja avaliações antes de decidir comprar!',
-	title_categories: ' é no Decidaki - veja avaliações antes de decidir comprar!',
-	title_reviews: ' - ver avaliações - Decidaki',
-	slogan: 'veja avaliações antes de decidir comprar.',
+	title: 'Decidaki - veja avaliações e cupons de descontos antes de decidir comprar!',
+	title_categories: ' é no Decidaki - veja avaliações e cupons de descontos antes de decidir comprar!',
+	title_reviews: ' - ver avaliações e cupons de descontos - Decidaki',
+	slogan:  'veja avaliações e cupons de descontos antes de decidir comprar.',
 };
+
+
